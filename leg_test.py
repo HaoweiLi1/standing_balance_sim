4852a547f710f32e3716825c988e52d2eb8f4127
celwell <celwell20@gmail.com>
2024-02-01 22:22:21 -0500

    a very naive torque controller with gravity compensation - no perturbation

--- leg_test.py
+++ leg_test.py
@@ -21,10 +21,14 @@ def controller(model, data):
     """
 
     if actuator_type == "torque":
-        model.actuator_gainprm[0, 0] = 10
+        model.actuator_gainprm[1, 0] = 1
+        # print(model.actuator_gainprm)
+        model.actuator_gainprm[0, 0] = 1
         data.ctrl[0] = -0.5 * \
             (data.sensordata[0] - 0.0) - \
             1 * (data.sensordata[1] - 0.0)
+        # print(model.actuator_gainprm)
+        # pass
         
     elif actuator_type == "servo":
         kp = 100.0
@@ -36,8 +40,10 @@ def controller(model, data):
         # gain_term = gain_prm[0] + gain_prm[1]*length + gain_prm[2]*velocity
 
         model.actuator_gainprm[1, 0] = kp
+        # print(model.actuator_gainprm)
         model.actuator_biasprm[1, 1] = -kp
-        data.ctrl[1] = -np.pi/4
+        print(model.actuator_biasprm)
+        data.ctrl[1] = -np.pi/8
 
         kv = 10.0
         model.actuator_gainprm[2, 0] = kv
@@ -60,16 +66,11 @@ def controller(model, data):
     # Random x-axis and y-axis perturbations applied to the shin
     # x_perturbation = np.random.normal(loc=0, scale=1)
     
-    y_perturbation = np.random.normal(loc=2.5, scale=2.5)
-
-    direction_flag = np.random.randint(low=0, high=1)
-
-    if direction_flag == 0:
-        y_perturbation *= -1
+    x_perturbation = np.random.normal(loc=10, scale=2.5)
 
     # Apply the body COM pertubations in Cartersian Space
     # data.xfrc_applied[i] = [ F_x, F_y, F_z, R_x, R_y, R_z]
-    # data.xfrc_applied[1] = [0, y_perturbation, 0., 0., 0., 0.]
+    # data.xfrc_applied[1] = [x_perturbation, 0, 0, 0., 0., 0.]
 
     # Apply joint perturbations in Joint Space
     # data.qfrc_applied = [ q_1, q_2, q_3, q_4, q_5, q_6, q_7, q_8]
@@ -147,9 +148,10 @@ context = mj.MjrContext(model, mj.mjtFontScale.mjFONTSCALE_150.value)
 # glfw.set_scroll_callback(window, scroll)
 
 #set initial conditions
-
-data.qpos[5] = np.pi/6
-data.qpos[6] = -np.pi/6
+data.qpos[0]=np.pi/8
+# data.qpos[1]=0
+# data.qpos[2]=-np.pi/6
+data.qpos[3]=-np.pi/6
 # print(data.qpos)
 # Set camera configuration
 cam.azimuth = 90.0
@@ -160,7 +162,7 @@ cam.lookat = np.array([0.012768, -0.000000, 1.254336])
 # perturbation_force = [0.0, 0.0, 1.0]  # Modify as needed
 # mj.MjData.xfrc_applied['shin_right'] = perturbation_force
 
-actuator_type = "servo"
+actuator_type = "torque"
 mj.set_mjcb_control(controller)
 
 while not glfw.window_should_close(window):
