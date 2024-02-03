4892dc74efe9ca2749b9f6b1233f7cf5906494ed
celwell <celwell20@gmail.com>
2024-02-02 22:00:17 -0500

    added code to modify XML script thru python

--- leg_test.py
+++ leg_test.py
@@ -6,9 +6,26 @@ from typing import Callable, Optional, Union, List
 import scipy.linalg
 import mediapy as media
 import matplotlib.pyplot as plt
+import threading
+import time
+from queue import Queue
+import xml.etree.ElementTree as ET
 
 xml_path = 'leg.xml'
 simend = 10
+
+def generate_large_impulse(perturbation_queue):
+    while True:
+        # Wait for a random amount of time (e.g., between 5 and 10 seconds)
+        wait_time = np.random.uniform(2, 3)
+        time.sleep(wait_time)
+
+        # Generate a large impulse
+        perturbation = np.random.uniform(30, 50)
+
+        # Put the generated impulse into the result queue
+        perturbation_queue.put(perturbation)
+        # print(perturbation)
 
 def controller(model, data):
     """
@@ -24,6 +41,8 @@ def controller(model, data):
         model.actuator_gainprm[1, 0] = 1
         # print(model.actuator_gainprm)
         model.actuator_gainprm[0, 0] = 1
+        # print(str(data.sensordata[0]) + ", " + str(data.sensordata[1]))
+        # print()
         data.ctrl[0] = -0.5 * \
             (data.sensordata[0] - 0.0) - \
             1 * (data.sensordata[1] - 0.0)
@@ -42,35 +61,24 @@ def controller(model, data):
         model.actuator_gainprm[1, 0] = kp
         # print(model.actuator_gainprm)
         model.actuator_biasprm[1, 1] = -kp
-        print(model.actuator_biasprm)
+        # print(model.actuator_biasprm)
         data.ctrl[1] = -np.pi/8
 
         kv = 10.0
         model.actuator_gainprm[2, 0] = kv
         model.actuator_biasprm[2, 2] = -kv
         data.ctrl[2] = 0.0
-
-        # model.actuator_gainprm[4, 0] = kp
-        # model.actuator_biasprm[4, 1] = -kp
-        # data.ctrl[4] = 0.0
-
-        # kv = 10.0
-        # model.actuator_gainprm[5, 0] = kv
-        # model.actuator_biasprm[5, 2] = -kv
-        # data.ctrl[5] = 0.0
-
-    # Random ankle perturbation applied to x-axis of ankle joint
-    # rx_perturbation = np.random.normal(loc=1.5, scale=0)
-    # ry_perturbation = np.random.normal(loc=1.5, scale=0)
-
-    # Random x-axis and y-axis perturbations applied to the shin
-    # x_perturbation = np.random.normal(loc=0, scale=1)
     
-    x_perturbation = np.random.normal(loc=10, scale=2.5)
-
+    x_perturbation=np.random.normal(loc=50, scale=2.5)
+    # if not perturbation_queue.empty():
+    #     x_perturbation = perturbation_queue.get()
+    
+    # print(x_perturbation)
+    # z_perburbation = np.random.normal(loc=12.5, scale=2.5)
     # Apply the body COM pertubations in Cartersian Space
+        
     # data.xfrc_applied[i] = [ F_x, F_y, F_z, R_x, R_y, R_z]
-    # data.xfrc_applied[1] = [x_perturbation, 0, 0, 0., 0., 0.]
+    data.xfrc_applied[1] = [-x_perturbation, 0, 0, 0., 0., 0.]
 
     # Apply joint perturbations in Joint Space
     # data.qfrc_applied = [ q_1, q_2, q_3, q_4, q_5, q_6, q_7, q_8]
@@ -118,11 +126,55 @@ def controller(model, data):
     ###############################################################
     ###############################################################
 
+def calculate_kp_and_geom(weight, height):
+    
+    M_total = weight # [kg]
+    H_total = height # [meters]
+    m_feet = 2 * 0.0145 * M_total
+    m_body = M_total - m_feet
+    l_COM = 0.575*H_total
+    l_foot = 0.152*H_total
+    a = 0.19*l_foot
+    K_p = m_body * 9.81 * l_COM
+
+    return m_feet, m_body, l_COM, l_foot, a, K_p
+
+    
+tree = ET.parse(xml_path)
+root = tree.getroot()
+# MODIFY THE XML FILE AND INSERT THE MASS AND LENGTH
+# PROPERTIES OF INTEREST WHICH ARE CALCULATED BY 
+# calculate_kp_and_geom(weight, height)
+M_total = 80 # kg
+H_total = 1.78 # meters
+m_feet, m_body, l_COM, l_foot, a, K_p = calculate_kp_and_geom( \
+                                                M_total, H_total)
+print(m_feet, m_body, l_COM, l_foot, a, K_p)
+
+for geom in root.iter('geom'):
+        if geom.get('name') == "shin_geom":
+            # Modify the mass attribute of the geom
+            geom.set('fromto', f'0 0 0 0 0 {-H_total}')
+            # geom.set('pos', f'0 0 {H_total-l_COM}')
+            geom.set('mass', str(m_body))
+            
+        elif geom.get('name') == "foot1_right":
+            geom.set('fromto', f'0 .02 0 {l_foot} .02 0')
+            geom.set('mass', str(m_feet))
+            # parent_body = geom.getparent()
+            # parent_body.set('pos', f'-{a} 0 -0.32')
+
+for body in root.iter('body'):
+        if body.get('name') == "le_foot":
+             body.set('pos',  f'-{a} 0 -{H_total+0.02}')
+
+tree.write('modified_model.xml')
 
 #get the full path
+modified_xml_path = 'modified_model.xml'
 script_directory = os.path.dirname(os.path.abspath(__file__))
 xml_path = os.path.join(script_directory, xml_path)
-model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
+model = mj.MjModel.from_xml_path(modified_xml_path)  # MuJoCo model
 # model = mj.MjData.from_xml_string(xml_path)
 
 data = mj.MjData(model)                # MuJoCo data
@@ -162,8 +214,19 @@ cam.lookat = np.array([0.012768, -0.000000, 1.254336])
 # perturbation_force = [0.0, 0.0, 1.0]  # Modify as needed
 # mj.MjData.xfrc_applied['shin_right'] = perturbation_force
 
+
+
+
 actuator_type = "torque"
 mj.set_mjcb_control(controller)
+
+perturbation_queue = Queue()
+
+perturbation_thread = threading.Thread \
+    (target=generate_large_impulse, 
+     daemon=True, 
+     args=(perturbation_queue,) )
+perturbation_thread.start()
 
 while not glfw.window_should_close(window):
     simstart = data.time
